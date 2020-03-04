48c7365ead0f6c11588a9d26a6d99a3795839dca
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2020-03-03 19:10:57 -0500

    movimiento en todas las imágenes

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,24 @@
 window.addEventListener('load', () => {
 
-    var cat1 = document.querySelector('.par-cats__floating--1');
+    var cats = document.querySelectorAll('.par-cats__floating');
+    var posYArr = [-200, -250, 150, 190];
+    var posXArr = [100, 470, 40, 570];
 
-    var handleScroll = (event) => {
-        var y = window.scrollY;
-        cat1.style.top = y / 2 - 200 + 'px';
-        cat1.style.transform = 'rotate(' + y/5 + 'deg)';
+    var iterateCats = (elem, index) => {
+
+        var handleScroll = (event) => {
+            var y = window.scrollY;
+            elem.style.top = y / 2 + posYArr[index] + 'px';
+            elem.style.left = y / 20 + posXArr[index] + 'px';
+            elem.style.transform = 'rotate(' + y/5 + 'deg)';
+        }
+        
+        handleScroll(); // inicializa la posición del gato
+        
+        window.addEventListener('scroll', handleScroll);
     }
-
-    handleScroll();
-
-    window.addEventListener('scroll', handleScroll);
+    cats.forEach(iterateCats);
+    
+    
 
 });
