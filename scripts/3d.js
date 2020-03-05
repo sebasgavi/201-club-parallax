92d9ee82e321d538c00ea75537173c971d345423
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2020-03-05 18:55:14 -0500

    3d room with happy man

--- scripts/3d.js
+++ scripts/3d.js
@@ -1,5 +1,13 @@
 function sketch3D (p) {
 
+    var happyManImage;
+    var roomWidth = p.windowWidth * 1.4;
+    var roomHeight = 600;
+
+    p.preload = function () {
+        happyManImage = p.loadImage('./images/happy-man.png');
+    }
+
     p.setup = function () {
         var canvas = p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
         canvas.parent('3d-space');
@@ -8,18 +16,30 @@ function sketch3D (p) {
     
     p.draw = function () {
         p.background(200);
-        //p.orbitControl();
+
+        var limit = .5;
+        var ry = p.map(p.mouseX, 0, p.windowWidth, -limit, limit);
 
         p.push();
+        p.rotateY(ry);
         p.translate(0, 300, 0);
         drawRoom();
+        drawContent(ry);
         p.pop();
     }
 
-    function drawRoom () {
-        var roomWidth = p.windowWidth * 1.4;
-        var roomHeight = 600;
-        
+    function drawContent (ry) {
+        // happy man
+        var w = 350;
+        p.push();
+        p.translate(w, w*-.5, 0);
+        p.rotateY(-ry);
+        p.texture(happyManImage);
+        p.plane(w, w);
+        p.pop();
+    }
+
+    function drawRoom () {        
         // atrás
         p.push();
         p.translate(0, roomHeight * -.5, roomHeight * -.5);
